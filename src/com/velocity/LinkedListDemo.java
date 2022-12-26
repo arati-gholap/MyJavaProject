e733192326ab14cc2c8ed8f17145f53bfba2e0eb
aratipawar <arati123gholap@gmail.com>
2022-12-26 16:30:52 +0530

    data added

---
+++ src/com/velocity/LinkedListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+public static void main(String[] args) {
+	
+	List<String> l=new LinkedList<String>();
+	l.add("arati");
+	l.add("rohini");
+	l.add("aasha");
+	l.add("aanu");
+	System.out.println(l);
+}
+}
